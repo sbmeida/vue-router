5dd9e2b74f38c7ca32a2d7dae91b3add84f8d99d
sbmeida <sbmeida@gmail.com>
2020-02-01 14:43:15 -0500

    Initial commit

---
+++ src/components/FirstRoute.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    Hello {{ $route.params.name }}
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "firstroute",
+  data() {
+    return {
+      msg: "FirstRoute"
+    };
+  }
+};
+</script>
---
+++ src/components/FirstRouteChild.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>test {{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "firstroutechild",
+  data() {
+    return {
+      msg: "FirstRouteChild"
+    };
+  },
+  mounted() {
+    console.log("mounted child");
+    //debugger;
+  }
+};
+</script>
